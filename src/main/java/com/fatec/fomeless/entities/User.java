be7da0880aa89fd9cff0234f659599599af71364
Vanessa Oliveira <tauchertvanessa@gmail.com>
2022-10-11 18:02:02 -0300

    user entitie lombok

--- src/main/java/com/fatec/fomeless/entities/User.java
+++ src/main/java/com/fatec/fomeless/entities/User.java
@@ -1,11 +1,18 @@
 package com.fatec.fomeless.entities;
 
-import javax.persistence.*;
-import java.util.Objects;
+import lombok.*;
 
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 @Entity
-@Table(name = "User")
-public abstract class User {
+@Table(name = "tb_users")
+public abstract class User implements Serializable {
     private static final Long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,51 +26,4 @@ public abstract class User {
 
     @JoinColumn(name = "email")
     protected Email email;
-
-    public User(){}
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-
-    public Email getEmail() {
-        return email;
-    }
-
-    public void setEmail(Email email) {
-        this.email = email;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return id.equals(user.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
