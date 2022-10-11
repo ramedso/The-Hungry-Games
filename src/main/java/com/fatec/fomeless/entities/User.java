a6de1928847a3c49021cab56fcf48d83885fc5dd
Rafael <rafael.sovza@gmail.com>
2022-10-11 17:23:28 -0300

    first commit

---
+++ src/main/java/com/fatec/fomeless/entities/User.java
@@ -0,0 +1,5 @@
+package com.fatec.fomeless.entities;
+
+public class User {
+
+}
---
+++ src/test/java/com/fatec/fomeless/FomelessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.fomeless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FomelessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
